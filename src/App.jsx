6ad29d87d581e62df827923bc544fb6d30cd3464
suzan-sheikh <mdsuzanskh@gmail.com>
2024-09-13 20:30:48 +0600

    chatboat added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import BannerComponent from "./Banner/BannerComponent";
-
-
+import ChatBot from 'react-simple-chatbot';
 
 const App = () => {
   return (
     <div>
-      <BannerComponent/>   
+      <BannerComponent />
+      <ChatBot
+        steps={[
+          {
+            id: '1',
+            message: 'What is your name?',
+            trigger: '2',
+          },
+          {
+            id: '2',
+            user: true,
+            trigger: '3',
+          },
+          {
+            id: '3',
+            message: 'Hi {previousValue}, nice to meet you!',
+            end: true,
+          },
+        ]}
+        floating={true}
+      />
     </div>
   );
 };
